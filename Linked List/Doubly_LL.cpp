0b8a993ac10c78bf10c7663ca789fa8a749949a0
Rawad Hossain <rawad.hossain00@gmail.com>
2024-03-21 23:16:24 +0600

    Update Doubly_LL.cpp

--- Linked List/Doubly_LL.cpp
+++ Linked List/Doubly_LL.cpp
@@ -184,6 +184,21 @@ void insert_before_node(Node *temp, int value) // Dont want to change head
     temp->back = newNode;
 }
 
+Node *reverseDLL(Node *head)
+{
+    if (head == NULL || head->next == NULL)
+        return head;
+    Node *last = NULL;
+    Node *current = head;
+    while (current != NULL)
+    {
+        last = current->back;
+        current->next = last;
+        current = current->back;
+    }
+    return last->back;
+}
+
 
 int main()
 {
